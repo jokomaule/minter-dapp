a318b353a9ddde564302b6bec43784864cce2f27
MrWommet <johannes-kroeger@web.de>
2022-09-29 22:37:18 +0200

    ok

--- backend/src/config.js
+++ backend/src/config.js
@@ -13,7 +13,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 const layerConfigurations = [
   {
-    growEditionSizeTo: 10000,
+    growEditionSizeTo: 50,
     layersOrder: [
       { name: "Background" },
       { name: "Fur" },
