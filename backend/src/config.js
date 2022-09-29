9e14b9258a72971aab53ea5111a6e0e8bbd5a3e5
MrWommet <johannes-kroeger@web.de>
2022-09-29 20:50:21 +0200

    ok

--- backend/src/config.js
+++ backend/src/config.js
@@ -70,7 +70,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x5DcE4A6a5FD78D5A71651d97c78A2cE0d8154404"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
